b9b3798248257bc3423bc8e0193e66bfe1ff0468
vyk1262 <vyk1262@gmail.com>
2023-06-15 12:39:09 +0530

    TM Pose Model with 2 classes

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:teachable/teachable.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
+  await Permission.microphone.request();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String pose = "";
+  String poseS = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Pose Classifier")),
+        body: Stack(
+          children: [
+            Container(
+                child: Column(children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: Teachable(
+                    path: "pose/index.html",
+                    results: (res) {
+                      var resp = jsonDecode(res);
+                      setState(() {
+                        pose = (resp['Bottle'] * 100.0).toString();
+                        poseS = (resp['Tree Pose'] * 100.0).toString();
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ])),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            pose,
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            poseS,
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+            )
+          ],
+        ));
+  }
+}
